3828c5644e9386551ea6cce4afe94ebc0614c7e8
eamdayan <eam.dayan@gmail.com>
2022-09-18 21:57:55 +0700

    added swagger ui to cloud gateway

--- api-gateway/src/main/kotlin/com/saintrivers/controltower/gateway/ApiGatewayApplication.kt
+++ api-gateway/src/main/kotlin/com/saintrivers/controltower/gateway/ApiGatewayApplication.kt
@@ -2,7 +2,6 @@ package com.saintrivers.controltower.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 
