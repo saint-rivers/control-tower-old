5d2768e8a74fee6100d69c313ee3ba6ee9df93dd
eamdayan <eam.dayan@gmail.com>
2022-09-18 22:08:53 +0700

    add eureka to config server

--- config-server/src/main/kotlin/com/saintrivers/controltower/configserver/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/com/saintrivers/controltower/configserver/ConfigServerApplication.kt
@@ -3,9 +3,11 @@ package com.saintrivers.controltower.configserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.context.config.annotation.RefreshScope
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 class ConfigServerApplication
 
 fun main(args: Array<String>) {
