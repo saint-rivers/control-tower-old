d939d4d76db5ec0fdd5023d8087d1fbf72377f67
eamdayan <eam.dayan@gmail.com>
2022-09-20 15:36:41 +0700

    fix keycloak client

--- keycloak-client/build.gradle.kts
+++ keycloak-client/build.gradle.kts
@@ -7,9 +7,11 @@ dependencies {
     {
         exclude("javax.ws.rs:jsr311-api")
     }
-    implementation("org.jboss.resteasy:resteasy-client")
-    implementation("org.jboss.resteasy:resteasy-jackson2-provider")
-    implementation("org.jboss.resteasy:resteasy-multipart-provider")
+//    implementation("org.jboss.resteasy:resteasy-client")
+//    implementation("org.jboss.resteasy:resteasy-jackson2-provider")
+//    implementation("org.jboss.resteasy:resteasy-multipart-provider")
+    implementation("org.glassfish.jersey.core:jersey-server:2.17")
+    implementation("org.glassfish.jersey.containers:jersey-container-servlet-core:2.17")
 
     implementation("org.keycloak:keycloak-admin-client:16.1.1")
     implementation("org.keycloak:keycloak-spring-boot-starter:16.1.1")
@@ -19,3 +21,16 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config:3.1.3")
     implementation(project(":common"))
 }
+
+
+//    <dependency>
+//        <groupId>org.glassfish.jersey.core</groupId>
+//        <artifactId>jersey-server</artifactId>
+//        <version>2.17</version>
+//    </dependency>
+//
+//    <dependency>
+//        <groupId>org.glassfish.jersey.containers</groupId>
+//        <artifactId>jersey-container-servlet-core</artifactId>
+//        <version>2.17</version>
+//    </dependency>
