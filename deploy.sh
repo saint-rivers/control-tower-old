9e5e63740b8337f60bebe83480d6655c167df31a
eamdayan <eam.dayan@gmail.com>
2022-09-20 16:36:22 +0700

    update gateway config

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 docker-compose down
 ./gradlew build -x test
-docker-compose up --scale task-service=2 --scale user-service=2 -d --build
+#docker-compose up --scale task-service=2 --scale user-service=2 -d --build
+docker-compose up -d --build
